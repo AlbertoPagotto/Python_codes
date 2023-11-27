4069ef9c183f58466153c4f38d3fb70ea2844ac0
AlbertoPagotto <148775192+AlbertoPagotto@users.noreply.github.com>
2023-11-27 11:58:35 +0100

    Update transfermarkt_download_data.py
    This is the working version of the script. It saves the file in a proper folder, some bugs have been avoided. Most of the 'try' checks can be deleted.

--- transfermarkt_download_data.py
+++ transfermarkt_download_data.py
@@ -1,7 +1,6 @@
 ###########################################
 # AUTHOR: Alberto Pagotto
-# Last version: 26/11/2023
-# To do: make the results saved in a specific folder. At the beginning check if the resulting file is already available
+# Last version: 27/11/2023
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -31,21 +30,28 @@ def element_clean(players):
 
     return [element[0:2]+element[3].split(' ')+element[4:] for element in players]
 
-current_directory=os.getcwd()
+current_directory=os.path.dirname(os.path.realpath(__file__))
+if not('data' in os.listdir(current_directory)):
+    os.mkdir('data')
+
 ## GENERATE THE URL TO ACCESS
-# season=season_fun()
-# transfer_window=transfer_window_fun()
-# position=position_fun()
-# age=input('What age are you interested in (all, u15-23, 23-30,o30-35)? ')
-season='2022/2023'
-transfer_window='all'
-position='all'
-age='all'
+season=season_fun()
+transfer_window=transfer_window_fun()
+position=position_fun()
+age=input('What age are you interested in (all, u15-23, 23-30,o30-35)? ')
+# season='2021/2022'
+# transfer_window='all'
+# position='all'
+# age='all'
 season_id=season.split('/')[0]
 transfer_id=transfer_dict[transfer_window.lower()]
 position_id=position_dict[position.lower()]
 age_id=age_dict[age.lower()]
 name_csv=f'Fees_{season_id}_{transfer_id}_{position_id}_{age_id}.csv'
+print(f'{current_directory}\data\{name_csv}')
+if os.path.exists(f'{current_directory}\data\{name_csv}'):
+    print('[WARNING] The file already exists. The script will be crashed.')
+    exit()
 url_season=f'https://www.transfermarkt.com/transfers/saisontransfers/statistik/top/plus/0/galerie/0?saison_id={season_id}transfers&transferfenster={transfer_id}&land_id=&ausrichtung={position_id}&spielerposition_id=&altersklasse={age}&leihe='
 
 
@@ -73,22 +79,18 @@ driver.switch_to.default_content()
 
 # Initialize variables
 pg=1
-odd_players=[]
-even_players=[]
-max_page=80 #max page number to read in case all the options are selected (not clear how to find it)
+data_transfer=[]
+max_page=2 #max page number to read in case all the options are selected (not clear how to find it)
 
 ## Find market values
 while (pg<=max_page):
     try:
         time.sleep(10)
         odd_players_page=driver.find_elements(by=By.CLASS_NAME,value='odd')
-        odd_players_page=[element.text.split('\n') for element in odd_players_page]
-        for odd_players_element in odd_players_page:
-            odd_players.append(odd_players_element)
         even_players_page=driver.find_elements(by=By.CLASS_NAME,value='even')
-        even_players_page=[element.text.split('\n') for element in even_players_page]
-        for even_players_element in even_players_page:
-            even_players.append(even_players_element)
+        players_page=[element.text.split('\n') for element in odd_players_page+even_players_page]
+        for players_element in players_page:
+            data_transfer.append(players_element)
         print(f"[INFO] Players found successfully at page {pg}.")
     except exceptions.NoSuchElementException:
         error_message(f'Failed to find players at page {pg}.')
@@ -100,41 +102,25 @@ while (pg<=max_page):
     pg+=1
     if pg>max_page: #Okay for now, but improvable maybe with recurrence
         break
-    for attempt in range(9):
-        try:
-            element_next = driver.find_element(By.CSS_SELECTOR, 'a.tm-pagination__link[title="Go to next page"]')
-            WebDriverWait(driver, 10).until(EC.element_to_be_clickable(element_next))
-            driver.execute_script("arguments[0].click();", element_next)
-            break
-        except exceptions.ElementClickInterceptedException:
-            print(f"Click intercepted on attempt {attempt + 1}")
-            time.sleep(1)
-        except exceptions.NoSuchElementException:
-            error_message(f'Failed to go to page {pg}. Button not found.')
-            break
-    #error_message(f'Failed to go to page {pg}. Element intecepted.')
+    try:
+        element_next = driver.find_element(By.CSS_SELECTOR, 'a.tm-pagination__link[title="Go to next page"]')
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(element_next))
+        driver.execute_script("arguments[0].click();", element_next)
+    except exceptions.NoSuchElementException:
+        error_message(f'Failed to go to page {pg}. Button not found.')
+        break
 
 ## Closing the driver
 driver.quit()
 print('[INFO] Driver closed successfully.')
 
-############################################################################################
-
 ## CLEAN THE DATA
-odd_players=element_clean(odd_players)
-even_players=element_clean(even_players)
-data_transfer=odd_players+even_players
+data_transfer=element_clean(data_transfer)
 
 ## CREATE A DATAFRAME
-# data_transfer=[]
-# for ii in range(0,len(odd_players+even_players)):
-#     if ii%2==0:
-#         data_transfer.append(odd_players[int(ii/2)])
-#     else:
-#         data_transfer.append(even_players[int((ii-1)/2)])
 df=pd.DataFrame(data=data_transfer,columns=['Rank','Name','Age','Value','Team','Championship','Fee'])
 df.drop('Rank',axis=1,inplace=True)
 
 ## CREATE a CSV FILE
-df.to_csv(name_csv,index=False)
+df.to_csv(f'{current_directory}\data\{name_csv}',index=False)
 print(f'[INFO] File {name_csv} written successfully.')
